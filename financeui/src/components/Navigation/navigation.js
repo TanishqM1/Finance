f6b6eafc6166fbda237b118eef99ca7e199d2006
Tanishq Mehta <tanishqmehta2004@gmail.com>
2024-09-09 23:15:14 -0700

    finished orb & created nav tab

---
+++ financeui/src/components/Navigation/navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+function Navigation() {
+    return(
+        <NavStyle>
+            
+        
+
+        </NavStyle>
+
+    )
+}
+
+const NavStyle = styled.div`
+
+
+
+`;
+
+export default Navigation
--- financeui/src/components/Orb/orb.js
+++ financeui/src/components/Orb/orb.js
@@ -1,7 +1,44 @@
-import React from "react";
-import styled from "styled-components";
+import React, {useState, useEffect} from "react";
+import styled, { keyframes } from "styled-components";
 
 function Orb() {
+
+    const generateRandomKeyframes = () => {
+        // Randomly generate positions for each keyframe (between 0 and the width/height of the screen)
+        const randomX = () => Math.floor(Math.random() * window.innerWidth) + "px";
+        const randomY = () => Math.floor(Math.random() * window.innerHeight) + "px";
+    
+        return keyframes`
+          0% {
+            transform: translate(${randomX()}, ${randomY()});
+          }
+          25% {
+            transform: translate(${randomX()}, ${randomY()});
+          }
+          50% {
+            transform: translate(${randomX()}, ${randomY()});
+          }
+          75% {
+            transform: translate(${randomX()}, ${randomY()});
+          }
+          100% {
+            transform: translate(${randomX()}, ${randomY()});
+          }
+        `;
+      };
+    
+      const [moveOrb, setMoveOrb] = useState(generateRandomKeyframes);
+    
+      // Re-generate the keyframes whenever the window is resized
+      useEffect(() => {
+        const handleResize = () => {
+          setMoveOrb(generateRandomKeyframes());
+        };
+        window.addEventListener("resize", handleResize);
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
 
   const OrbStyled = styled.div`
    
@@ -9,10 +46,12 @@ function Orb() {
     height: 70vh;
     position: absolute;
     border-radius: 50%;
-    margin-top: -37vh;
-    margin-left: -37vh;
-    background: linear-gradient(180deg, grey 0%, black 100%);
-    filter: blur(5px);
+    border: 2px solid #000;
+    margin-top: -45vh;
+    margin-left: -45vh;
+    background: linear-gradient(90deg, rgba(73, 72, 72, 0.8) 0%, black 100%);
+    filter: blur(80px);
+    animation: ${moveOrb} 10s alternate linear infinite;
   `;
 
   return <OrbStyled></OrbStyled>;
