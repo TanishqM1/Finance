20948f0d9d9f2a8963fb78fdbc5747b82215a209
Tanishq Mehta <tanishqmehta2004@gmail.com>
2024-11-26 19:43:35 -0800

    icons

--- financeui/src/utils/icons.js
+++ financeui/src/utils/icons.js
@@ -1,11 +1,14 @@
-export const Taskbar = <i className="fa-solid fa-house"></i>    // taskbar icon
+// icons used for the navigation bar
+export const Taskbar = <i className="fa-solid fa-house"></i>
 export const Transaction = <i className="fa-solid fa-wallet"></i>
 export const Income = <i className="fa-solid fa-money-bill"></i>
 export const Expense = <i className="fa-solid fa-hand-holding-dollar"></i>
 export const signout = <i className="fa-solid fa-right-from-bracket"></i>
+
+//used for add button
 export const addicon = <i class="fa-solid fa-plus"></i>
 
-// initially used in incomeitem.js
+// icons used in income.js form
 export const dollar = <i class="fa-solid fa-dollar-sign"></i>
 export const calender = <i class="fa-solid fa-calendar-days"></i>
 export const comment = <i class="fa-regular fa-comment"></i>
