da7c6b481477c3340573e5376e9ef62918d8d785
Tanishq Mehta <tanishqmehta2004@gmail.com>
2024-11-26 18:16:21 -0800

    added input for incomes and icons for incomeitem

--- financeui/src/components/Incomes/Incomes.js
+++ financeui/src/components/Incomes/Incomes.js
@@ -3,14 +3,16 @@ import styled from "styled-components";
 import { InnerLayout } from "../../styles/layout";
 import { UseGlobalContext } from "../../context/globalcontext";
 import Form from '../Form/form'
+import IncomeItem from "./IncomeItem";
 
 function Incomes() {
     const {addIncome, incomes, getIncome} = UseGlobalContext()
 
+    // Runs on initial render
     useEffect(()=> {
         getIncome();
     }, [])
-    
+
     return (
         <IncomesStyled>
             <InnerLayout>
@@ -21,6 +23,10 @@ function Incomes() {
                         <Form/>
                     </div>
                     <div class="incomes">
+                        {incomes.map((income) =>{
+                            const {id, title, amount, date, category, description} = income;
+                            return <IncomeItem></IncomeItem>
+                        })}
                         
                     </div>
                 </div>
--- financeui/src/utils/icons.js
+++ financeui/src/utils/icons.js
@@ -1,6 +1,12 @@
-export const Taskbar = <i className="fa-solid fa-house"></i>
+export const Taskbar = <i className="fa-solid fa-house"></i>    // taskbar icon
 export const Transaction = <i className="fa-solid fa-wallet"></i>
 export const Income = <i className="fa-solid fa-money-bill"></i>
 export const Expense = <i className="fa-solid fa-hand-holding-dollar"></i>
 export const signout = <i className="fa-solid fa-right-from-bracket"></i>
 export const addicon = <i class="fa-solid fa-plus"></i>
+
+// initially used in incomeitem.js
+export const dollar = <i class="fa-solid fa-dollar-sign"></i>
+export const calender = <i class="fa-solid fa-calendar-days"></i>
+export const comment = <i class="fa-regular fa-comment"></i>
+export const garbage = <i class="fa-solid fa-trash"></i>
