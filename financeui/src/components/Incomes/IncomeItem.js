27f354bf7041a99306d7f202546297cb710a0c2c
Tanishq Mehta <tanishqmehta2004@gmail.com>
2024-11-26 18:22:59 -0800

    added styling to incomeitem

--- financeui/src/components/Incomes/IncomeItem.js
+++ financeui/src/components/Incomes/IncomeItem.js
@@ -47,7 +47,17 @@ function IncomeItem({
 }
 
 const IncomeItemStyled = styled.div`
-
+    background: #28282A;
+    border: 2px solid #FFFFFF;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    border-radius: 20px;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    width: 100%;
+    color: red;
 `;
 
 export default IncomeItem
